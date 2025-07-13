fefbe3fad932a01ae5cf0c66e377d37c682f1708
J0K34 <146594936+j0k34sec@users.noreply.github.com>
2025-07-13 16:01:19 +0530

    Refactor Student class initialization and remove commented-out code

--- python-course/test.py
+++ python-course/test.py
@@ -1,5 +1,21 @@
-class Student:
-    name = "suraj mondal"
+# class Student:
+#     name = "suraj mondal"
 
-s1 = Student()
-print(s1)  
+# s1 = Student()
+# print(s1)  
+
+class Student:
+    def __init__(self, first_param, second_param):
+        # print("this is class")
+        # self.fullname = fullname
+        # self.gfname = gfname
+        # print(self.gfname)
+        # print(self.fullname) 
+        self.first_param = first_param
+        self.second_param = second_param
+        print(self.first_param)
+        print(self.second_param)
+# s1 = Student("Suraj Mondal,", "my girlfriend's name is tiya")
+# s2 = Student("this is my second object", None)
+s1 = Student("Suraj Mondal", 1)
+s2 = Student("My girlfriend's name is Tiya", 2)

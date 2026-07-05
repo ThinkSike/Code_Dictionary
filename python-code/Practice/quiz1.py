9d336097c98483b7d0fe4f3297bce77818540fc3
ThinkSike <138986417+ThinkSike@users.noreply.github.com>
2026-07-05 12:22:14 +0530

    Add files via upload

---
+++ python-code/Practice/quiz1.py
@@ -0,0 +1,39 @@
+# x = 15
+# if x > 10:
+#     if x < 20:
+#         print("A")
+#     else:
+#         print("B")
+# else:
+#     print("C")
+
+# x = 5
+# y = 10
+# z = 15
+# if x > y and x > z:
+#     print("A")
+# elif y > x and y > z:
+#     print("B")
+# else:
+#     print("C")
+
+# s = "Python Programming"
+# print(s[7:11])
+
+# s = "Hello, World!"
+# print(s.replace("o", "a").upper())
+
+# s = "Python"
+# print(s[::-1])
+
+# s = "Python"
+# print(s.find("th"))
+
+# x = 10
+# y = 20
+# if x > y:
+#     print("A")
+# elif x == y:
+#     print("B")
+# else:
+#     print("C")
